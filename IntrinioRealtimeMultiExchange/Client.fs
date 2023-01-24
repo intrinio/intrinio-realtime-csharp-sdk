c5f8ba75e0b7da3f4088c4db0e38a65027860130
ssnyder <ssnyder@intrinio.com>
2023-01-24 15:54:19 -0600

    Version bump

--- IntrinioRealtimeMultiExchange/Client.fs
+++ IntrinioRealtimeMultiExchange/Client.fs
@@ -327,7 +327,7 @@ type Client(onTrade : Action<Trade>, onQuote : Action<Quote>) =
 
     do
         httpClient.Timeout <- TimeSpan.FromSeconds(5.0)
-        httpClient.DefaultRequestHeaders.Add("Client-Information", "IntrinioDotNetSDKv4.0")
+        httpClient.DefaultRequestHeaders.Add("Client-Information", "IntrinioDotNetSDKv4.1")
         tryReconnect <- fun () ->
             let reconnectFn () : bool =
                 Log.Information("Websocket - Reconnecting...")
