434aa632abd6a62a654462e3248e5f26d1cc7575
Shawn Snyder <ssnyder@intrinio.com>
2024-08-16 01:10:56 -0500

    progress

---
+++ Intrinio.Realtime/Equities/ClientStats.cs
@@ -0,0 +1,51 @@
+namespace Intrinio.Realtime.Equities;
+
+public class ClientStats
+{
+    private readonly UInt64 _socketDataMessages;
+    private readonly UInt64 _socketTextMessages;
+    private readonly int _queueDepth;
+    private readonly UInt64 _eventCount;
+    private readonly UInt64 _tradeCount;
+    private readonly UInt64 _quoteCount;
+
+    public ClientStats(UInt64 socketDataMessages, UInt64 socketTextMessages, int queueDepth, UInt64 eventCount, UInt64 tradeCount, UInt64 quoteCount)
+    {
+        _socketDataMessages = socketDataMessages;
+        _socketTextMessages = socketTextMessages;
+        _queueDepth = queueDepth;
+        _eventCount = eventCount;
+        _tradeCount = tradeCount;
+        _quoteCount = quoteCount;
+    }
+
+    public UInt64 SocketDataMessages()
+    {
+        return _socketDataMessages;
+    }
+    
+    public UInt64 SocketTextMessages()
+    {
+        return _socketTextMessages;
+    }
+    
+    public int QueueDepth()
+    {
+        return _queueDepth;
+    }
+    
+    public UInt64 EventCount()
+    {
+        return _eventCount;
+    }
+    
+    public UInt64 TradeCount()
+    {
+        return _tradeCount;
+    }
+    
+    public UInt64 QuoteCount()
+    {
+        return _quoteCount;
+    }
+}
---
+++ Intrinio.Realtime/Equities/IEquitiesWebSocketClient.cs
@@ -0,0 +1,14 @@
+namespace Intrinio.Realtime.Equities;
+
+public interface IEquitiesWebSocketClient
+{
+    public void Join();
+    public void Join(string channel, bool? tradesOnly);
+    public void Join(string[] channels, bool? tradesOnly);
+    public void Leave();
+    public void Leave(string channel);
+    public void Leave(string[] channels);
+    public void Stop();
+    public ClientStats GetStats();
+    public void Log(string message, params object[] parameters);
+}
