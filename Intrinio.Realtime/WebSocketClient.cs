968d1bed61ec27536768b30bb6f2ad2cf5cad3a1
ssnyder-intrinio <87659980+ssnyder-intrinio@users.noreply.github.com>
2025-01-13 09:01:04 -0600

    fix tick (#56)

--- Intrinio.Realtime/WebSocketClient.cs
+++ Intrinio.Realtime/WebSocketClient.cs
@@ -36,7 +36,7 @@ public abstract class WebSocketClient
     private readonly Func<Task> _tryReconnect;
     private readonly HttpClient _httpClient = new ();
     private const string ClientInfoHeaderKey = "Client-Information";
-    private const string ClientInfoHeaderValue = "IntrinioDotNetSDKv12.6";
+    private const string ClientInfoHeaderValue = "IntrinioDotNetSDKv12.7";
     private readonly ThreadPriority _mainThreadPriority;
     private readonly Thread[] _threads;
     private Thread? _receiveThread;
