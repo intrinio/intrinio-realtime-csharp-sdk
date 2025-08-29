c9c88255ccb67815a34615ecb32eaf985f0926dd
Shawn Snyder <ssnyder@intrinio.com>
2025-08-29 08:14:00 -0500

    version bump

--- Intrinio.Realtime/WebSocketClient.cs
+++ Intrinio.Realtime/WebSocketClient.cs
@@ -37,7 +37,7 @@ public abstract class WebSocketClient
     private readonly Func<Task>               _tryReconnect;
     private readonly HttpClient               _httpClient           = new ();
     private const    string                   ClientInfoHeaderKey   = "Client-Information";
-    private const    string                   ClientInfoHeaderValue = "IntrinioDotNetSDKv16.1";
+    private const    string                   ClientInfoHeaderValue = "IntrinioDotNetSDKv16.2";
     private readonly ThreadPriority           _mainThreadPriority;
     private readonly Thread[]                 _threads;
     private          Thread?                  _receiveThread;
